4e5970f1da1e6c3de98b8408a28e7a1ee71eb764
Utsavlamgade1 <utsav.lamgade@codeimmersives.com>
2020-12-28 21:40:58 -0500

    Adding Git Ignore

--- src/java/practice/DummyClassByUT.java
+++ src/java/practice/DummyClassByUT.java
@@ -1,2 +1,7 @@
-package java.practice;public class DummyClassByUT {
+package java.practice;
+
+public class DummyClassByUT {
+    public static void main(String[] args) {
+        System.out.println("This is EagleEye!!!");
+    }
 }
