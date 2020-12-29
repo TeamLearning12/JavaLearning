a7892552f7991f70cf1696af89ecbcd239598a03
Sandesh <team.learning12@gmail.com>
2020-12-29 11:57:25 -0700

    Modified output

--- src/java/practice/Random.java
+++ src/java/practice/Random.java
@@ -1,2 +1,7 @@
-package java.practice;public class Random {
+package java.practice;
+
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("I am going crazy");
+    }
 }
